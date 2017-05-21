8fbb312b451c954e9c2b83d0623e8bee01cd7857
Wanasak <u510610433@gmail.com>
2017-05-22 00:40:43 +0700

    using automapper

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using vega_demo.Controllers.Resources;
 using vega_demo.Models;
 using vega_demo.Persistence;
 
@@ -10,15 +12,18 @@ namespace vega_demo.Controllers
     public class MakesController : Controller
     {
         private readonly VegaDbContext context;
-        public MakesController(VegaDbContext context)
+        private readonly IMapper mapper;
+        public MakesController(VegaDbContext context, IMapper mapper)
         {
+            this.mapper = mapper;
             this.context = context;
         }
 
         [HttpGet("/api/makes")]
-        public async Task<IEnumerable<Make>> GetMakes()
+        public async Task<IEnumerable<MakeResource>> GetMakes()
         {
-            return await context.Makes.Include(m => m.Models).ToListAsync();
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
         }
     }
 }
---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using vega_demo.Controllers.Resources;
+using vega_demo.Models;
+
+namespace vega_demo.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
