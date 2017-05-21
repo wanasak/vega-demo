4a2d68027c5484ac5e5d6aebcb56f34c3029d9c2
Wanasak <u510610433@gmail.com>
2017-05-21 23:24:11 +0700

    creating the database

---
+++ Migrations/VegaDbContextModelSnapshot.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using vega_demo.Persistence;
+
+namespace VegaDemo.Migrations
+{
+    [DbContext(typeof(VegaDbContext))]
+    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("vega_demo.Models.Make", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Makes");
+                });
+
+            modelBuilder.Entity("vega_demo.Models.Model", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("MakeId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("MakeId");
+
+                    b.ToTable("Models");
+                });
+
+            modelBuilder.Entity("vega_demo.Models.Model", b =>
+                {
+                    b.HasOne("vega_demo.Models.Make", "Make")
+                        .WithMany("Models")
+                        .HasForeignKey("MakeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using vega_demo.Models;
 
 namespace vega_demo.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+        
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
         {
             
