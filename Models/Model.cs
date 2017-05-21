c852daedac803087e380de671782f05ac40c1264
Wanasak <u510610433@gmail.com>
2017-05-21 22:49:42 +0700

    building the main model

---
+++ Models/Model.cs
@@ -0,0 +1,10 @@
+namespace vega_demo.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
+    }
+}
