7b1fb5d9697cd908f74cb799b9518d84408b55b6
Wanasak <u510610433@gmail.com>
2017-05-22 00:11:40 +0700

    seeding the database

---
+++ Migrations/20170521170041_SeedDatabase.Designer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using vega_demo.Persistence;
+
+namespace VegaDemo.Migrations
+{
+    [DbContext(typeof(VegaDbContext))]
+    [Migration("20170521170041_SeedDatabase")]
+    partial class SeedDatabase
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("vega_demo.Models.Make", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Make");
+                });
+
+            modelBuilder.Entity("vega_demo.Models.Model", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("MakeId");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(255);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("MakeId");
+
+                    b.ToTable("Model");
+                });
+
+            modelBuilder.Entity("vega_demo.Models.Model", b =>
+                {
+                    b.HasOne("vega_demo.Models.Make", "Make")
+                        .WithMany("Models")
+                        .HasForeignKey("MakeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ Migrations/20170521170041_SeedDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VegaDemo.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Make (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Make (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Make (Name) VALUES ('Make3')");
+
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make1-ModelA', (SELECT Id FROM Make WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make1-ModelB', (SELECT Id FROM Make WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make1-ModelC', (SELECT Id FROM Make WHERE Name = 'Make1'))");
+
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make2-ModelA', (SELECT Id FROM Make WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make2-ModelB', (SELECT Id FROM Make WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make2-ModelC', (SELECT Id FROM Make WHERE Name = 'Make2'))");
+
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make3-ModelA', (SELECT Id FROM Make WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make3-ModelB', (SELECT Id FROM Make WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Model (Name, MakeId) VALUES ('Make3-ModelC', (SELECT Id FROM Make WHERE Name = 'Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Make");
+        }
+    }
+}
