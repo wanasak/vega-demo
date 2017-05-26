e1ad374712fd4b82cc78aecdf9ba2b20042fddd4
wanasak <u510610433@gmail.com>
2017-05-26 11:00:01 +0700

    adding the filters

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VehicleService } from "../../services/vehicle.service";
-import { Vehicle } from "../../models/vehicle";
+import { Vehicle, KeyPairValue } from "../../models/vehicle";
 
 @Component({
   selector: 'app-vehicle-list',
@@ -9,12 +9,21 @@ import { Vehicle } from "../../models/vehicle";
 export class VehicleListComponent implements OnInit {
 
   vehicles: Vehicle[];
+  makes: KeyPairValue[];
+  filter: any = {};
 
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
+    this.vehicleService.getMakes()
+      .subscribe(makes => this.makes = makes);
+
     this.vehicleService.getVehicles()
       .subscribe(vehicles => this.vehicles = vehicles);
+  }
+
+  onFilterChange() {
+    
   }
 
 }
