dddaa75deeed84ab593924dc37462033fe93fc98
wanasak <u510610433@gmail.com>
2017-05-26 15:38:41 +0700

    sending the sort parameters to server

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -11,7 +11,7 @@ export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
   allVehicles: Vehicle[];
   makes: any[];
-  filter: any = {};
+  query: any = {};
 
   constructor(private vehicleService: VehicleService) { }
 
@@ -23,7 +23,7 @@ export class VehicleListComponent implements OnInit {
   }
 
   private populateVehicles() {
-    this.vehicleService.getVehicles(this.filter)
+    this.vehicleService.getVehicles(this.query)
       .subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
   }
 
@@ -41,8 +41,18 @@ export class VehicleListComponent implements OnInit {
   }
 
   resetFilter() {
-    this.filter = {};
+    this.query = {};
     this.onFilterChange();
+  }
+
+  sortBy(columnName) {
+    if (this.query.sortBy === columnName) {
+      this.query.isSortAscending = false;
+    } else {
+      this.query.sortBy = columnName;
+      this.query.isSortAscending = true;
+    }
+    this.populateVehicles();
   }
 
 }
